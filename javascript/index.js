2e827be7af4dd214666861ad2a62df1393ee3673
KSHITIJA-PISAL <kshitijapisal41@gmail.com>
2024-11-07 16:50:32 +0530

    Live date and time of 2 cities

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,25 @@
-alert("Hello!");
+function citiTime() {
+  // Los Angeles
+  let losAngelesElement = document.querySelector("#losAngeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  // Australia
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+citiTime();
+setInterval(citiTime, 1000);
